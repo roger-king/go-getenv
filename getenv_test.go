4b29982090df74625eee76e4dc16e04ee1fba71a
Roger King <rogr.king@gmail.com>
2020-05-17 16:17:45 -0400

    chore: testing

---
+++ getenv_test.go
@@ -0,0 +1,38 @@
+package getenv
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+var (
+	expectedDefaultValue = ":5000"
+)
+
+type GetEnvSuite struct {
+	suite.Suite
+}
+
+func (s *GetEnvSuite) TestGetEnvReturnsDefault_Succeeds() {
+	value := GetEnv("PORT", String(expectedDefaultValue))
+
+	s.Equal(expectedDefaultValue, value)
+}
+
+func (s *GetEnvSuite) TestGetEnvReturnsEnvOverDefault_Succeeds() {
+	os.Setenv("PORT", ":9000")
+
+	value := GetEnv("PORT", String(expectedDefaultValue))
+
+	s.Equal(":9000", value)
+}
+
+func (s *GetEnvSuite) TestGetEnvPanics_Succeeds() {
+	s.Assert().Panics(func() { GetEnv("PORT", Nil()) }, "Expected panic of missing environment variable")
+}
+
+func TestGetEnvSuite(t *testing.T) {
+	suite.Run(t, new(GetEnvSuite))
+}
