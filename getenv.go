bbd623a04134f4c13fc194aa79d2cab05de73fdc
Roger King <rogr.king@gmail.com>
2020-05-17 16:17:03 -0400

    feat: getenv functions

--- getenv.go
+++ getenv.go
@@ -1,9 +1,39 @@
 package getenv
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
-// GetEnv -
-func GetEnv(args ...string) string {
-	fmt.Println(args)
-	return ""
+// DefaultString -
+type DefaultString *string
+
+// GetEnv - main function to fetch for environment variable or return default
+func GetEnv(key string, d DefaultString) string {
+	value := os.Getenv(key)
+	if len(value) == 0 && d != nil {
+		log.Print(fmt.Sprintf("%s is not found. fallback to default value.", key))
+		return Value(d)
+	} else if len(value) == 0 && d == nil {
+		log.Panicf("%s is a required environment variable", key)
+	}
+
+	return value
+}
+
+// Nil - custom fn to return nil
+func Nil() *string {
+	return nil
+}
+
+// String - custom fn to return the pointer of a string
+func String(s string) *string {
+	newS := s
+	return &newS
+}
+
+// Value - returns the default value as a string
+func Value(defaultValue DefaultString) string {
+	return *defaultValue
 }
